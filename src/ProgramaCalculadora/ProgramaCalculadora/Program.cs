cd6c8663f3a520fe188092a2b3c3779ca3212423
naiaragabriela <naigabi31@gmail.com>
2023-03-06 01:03:14 -0300

    criando calculadora

--- src/ProgramaCalculadora/ProgramaCalculadora/Program.cs
+++ src/ProgramaCalculadora/ProgramaCalculadora/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿float n1, n2;
+
+Console.WriteLine("Digite o primeiro número para a calculadora realizar as operações");
+n1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite o primeiro número para a calculadora realizar as operações");
+n2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma dos números digitados é: " + (n1 + n2));
+Console.WriteLine("A subtração dos números digitados é: " + (n1 - n2));
+Console.WriteLine("A multiplicação dos números digitados é: " + (n1 * n2));
+
+if (n2 == 0)
+{
+    Console.WriteLine("Impossível dividir por zero!");
+
+}
+else
+{
+    Console.WriteLine("A divisão dos números é: " + (n1 / n2));
+}
+
+Console.ReadKey();
